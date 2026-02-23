e6573b2fd1e8fe0bfa74664f87d9fa55b7f8f22e
Satyam Kumar Choudhary <choudharysatyam951478@gmail.com>
2026-02-24 03:38:12 +0530

    File Update: pom.xml ===> Dependency Managed ===> Build Success for initial Stage

---
+++ ghostlink/src/test/java/io/ghostlink/AppTest.java
@@ -0,0 +1,20 @@
+package io.ghostlink;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
